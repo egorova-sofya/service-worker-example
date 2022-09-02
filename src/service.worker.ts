e78bfd09cdebcbceeb6c351d1accb07bcf17ae4f
egorova-sofya <egorovaSjob@yandex.ru>
2022-09-02 17:58:00 +0300

    created activate phase

--- src/service.worker.ts
+++ src/service.worker.ts
@@ -4,7 +4,7 @@ const version = 'v0.0.4';
 // declare var self: ServiceWorkerGlobalScope;
 // declare var self: ServiceWorkerGlobalScope;
 
-const cashKey = 'cashData';
+const cashKey = 'cashData_v0.0.1';
 
 _self.addEventListener('install', (e) => {
   //фаза install кеширует необходимы для работы сервис воркера ресурсы
@@ -12,6 +12,18 @@ _self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(cashKey).then((cache) => {
       return cache.addAll(['/']);
+    })
+  );
+});
+
+_self.addEventListener('activate', (e) => {
+  //чистка кэша
+  console.log('install');
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.filter((key) => key !== cashKey).map((key) => caches.delete(key))).then(() =>
+        console.log(keys)
+      );
     })
   );
 });
